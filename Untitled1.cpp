e1ac9a44d0b2089909ba67b7975f2511314734e4
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-05 18:53:16 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,212 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int sum=a+=b;
+//cout<<"sum is "<<sum;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int sub=a-=b;
+//cout<<"substraction is "<<sub;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int mult=a*=b;
+//cout<<"multiplication is "<<mult;}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int div=a/=b;
+//cout<<"division is "<<div;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int mod=a%=b;
+//cout<<"mod is "<<mod;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int store=a<b&&a>b;
+//cout<<"answer is "<<store;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//int store=a<b||a>b;
+//cout<<"answer is "<<store;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a=5,
+//b=10;
+//int store=a<b&&a>b;
+//cout<<"answer is "<<store;}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a=23,
+//b=9;
+//cout<<"jhjgh"<<(a<b);
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//cout<<"answer is "<<!(a<b);
+//}
+
+//QUESTIONS 
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter four numbers";
+//cin>>a>>b;
+//if(a==b)
+//cout<<"equal";
+//else
+//cout<<"not equal";
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//cout<<"answer is "<<(a<50&&a<b);
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//cout<<"answer is "<<(a<50||a<b);
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a;
+//cout<<"enter four digit number";
+//cin>>a;
+//int sum=a+8;
+//int div=sum/3;
+//int mod=div%5;
+//int mult=mod*5;
+//cout<<"answer is "<<mult;}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int a,b;
+//cout<<"enter two numbers";
+//cin>>a>>b;
+//cout<<"answer is "<<(a==b);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
