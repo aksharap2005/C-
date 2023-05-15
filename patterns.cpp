20fbac7e9cb192bc549f67d28a55c09d44d05b44
aksharap2005 <131359971+aksharap2005@users.noreply.github.com>
2023-05-15 18:40:17 +0530

    Add files via upload

---
+++ patterns.cpp
@@ -0,0 +1,81 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int i,j;
+//for(i=0;i<5;i++)
+//{for(j=0;j<i;j++)
+//cout<<"*";
+//cout<<"\n";
+//}
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int i,j;
+//for(i=0;i<5;i++)
+//{for(j=0;j<5;j++)
+//cout<<"*";
+//cout<<"\n";
+//}
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int i,j;
+//for(i=0;i<5;i++)
+//{for(j=0;j<i;j++)
+//cout<<i;
+//cout<<"\n";
+//}
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{int i,j;
+//for(i=0;i<5;i++)
+//{for(j=0;j<i;j++)
+//cout<<j;
+//cout<<"\n";
+//}
+//}
+
+#include<iostream>
+using namespace std;
+int main()
+{int i,j;
+for(i=0;i<6;i++)
+{for(j=0;j<i;j++)
+cout<<"*";
+cout<<"\n";
+}
+for(i=5;i>0;i--)
+{for(j=0;j<i;j++)
+cout<<"*";
+cout<<"\n";
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
